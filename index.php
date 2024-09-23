454ffd695be8fc84ed8478657a976c318a02f805
Polidowze <167921430+Polidowze@users.noreply.github.com>
2024-09-23 20:35:12 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <form action="script.php" method="post">
+    
+    <label for="nome">Nome</label>
+    <input type="text" name="nome" id="nome">
+    <br>
+    <label for="nota1">Nota1</label>
+    <input type="number" name="nota1" id="nome">
+    <br>
+    <label for="nota2">Nota2</label>
+    <input type="number" name="nota2" id="nome">
+    <br>
+    <label for="nota3">Nota3</label>
+    <input type="number" name="nota3" id="nome">
+    <br>
+    <label for="nota4">Nota4</label>
+    <input type="number" name="nota4" id="nome">
+    <br>
+    <label for="nota5">Nota5</label>
+    <input type="number" name="nota5" id="nome">
+    <br>
+    <button type="submit">Enviar</button>
+    </form>
+    
+</body>
+</html>
---
+++ script.php
@@ -0,0 +1,23 @@
+ <?php 
+//print_r($_POST);//
+$nome = $_POST['nome'];
+$nota1 = $_POST['nota1'];
+$nota2 = $_POST['nota2'];
+$nota3 = $_POST['nota3'];
+$nota4 = $_POST['nota4'];
+
+$media = ($nota1 + $nota2 + $nota3 + $nota4) / 4;
+
+echo "Meu nome é $nome e Minha nota final é $media";
+echo "<br>";
+if ($media>=6) {
+    echo "O aluno $nome, está Aprovado!";
+}elseif($media>=4 && $media<6){
+    echo "Recuperação";
+}
+else {
+    echo "O aluno $nome, está Reprovado!";
+}
+
+
+?> 
